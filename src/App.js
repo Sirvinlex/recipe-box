b11b0366d03c5c6d6b2490c41ad3a8036573a9b4
sirvinlex <vinlex3030@gmail.com>
2023-10-12 12:46:40 +0100

    completed and improved edit and delete functionalities, modified creation of recipe functionality

--- src/App.js
+++ src/App.js
@@ -24,19 +24,20 @@ function App() {
 
   const handleSubmit = (e) =>{
     e.preventDefault();
-    const id = Math.ceil((Date.now() * Math.random()) + 1);
-    // setData([...localData, {recipe, ingredients, directions, id}])
-    // there will be a temporary state tha will render in the UI immediately before local storage is updated. 
-    // This temp state should also be used to update the local storage, because if someone add stuff multiple times 
-    // without refreshing page the localData wont be updated
-    setLocalData([...localData,  {recipe, ingredients, directions, id}])
-    
-    // localStorage.setItem("info", JSON.stringify([...localData, {recipe, ingredients, directions, id}]));
-    // arr.push({recipe, ingredients, directions, id});
+    if (!recipe || !ingredients || !directions) alert('Please fill all fields');
+    else{
+      const id = Math.ceil((Date.now() * Math.random()) + 1);
+      setLocalData([...localData,  {recipe, ingredients, directions, id}]);
+      if(localData.length === 0) setShow({recipe, ingredients, directions, id});
+      setRecipe('');
+      setIngredients('');
+      setDirections('');
+    }
   }
 
   React.useEffect(() =>{
     localStorage.setItem("info", JSON.stringify(localData));
+    // if(localData.length === 1) setShow(localData[0]);
   }, [localData])
   const handleRecipe = (e) =>{
    setRecipe(e.target.value);
@@ -52,9 +53,9 @@ function App() {
     const _id = e.target.id;
     const updated = localData.filter((item) => item.id != _id)
     setLocalData(updated);
-    setShow(localData[0]);
+    setShow({});
   }
-
+  // const set = () => setShow(localData[0])
   const handleEdit = (e) =>{
     setIsEdit(true);
     const _id = e.target.id;
@@ -69,36 +70,41 @@ function App() {
   }
 
   const submitEdit = (e) =>{
-    e.preventDefault()
-    const toBeUpdated = localData.concat([]);
+    e.preventDefault();
+    if (!recipe || !ingredients || !directions) alert('Please fill all fields');
+    else{
+      const toBeUpdated = localData.concat([]);
 
-    toBeUpdated.map((item) =>{
-      if(item.id == editId){
-        item.recipe = recipe;
-        item.ingredients = ingredients;
-        item.directions = directions;
-      }
-    })
-    console.log(toBeUpdated, 'tobe')
-    setLocalData(toBeUpdated);
-    setRecipe('');
-    setIngredients('');
-    setDirections('');
-    setEditId('');
-    setIsEdit(false);
+      toBeUpdated.map((item) =>{
+        if(item.id == editId){
+          item.recipe = recipe;
+          item.ingredients = ingredients;
+          item.directions = directions;
+        }
+      })
+      setLocalData(toBeUpdated);
+      setRecipe('');
+      setIngredients('');
+      setDirections('');
+      setEditId('');
+      setIsEdit(false);
+    }
   }
 
+  const displayedItem = show ? show : {};
+  const ingr = Object.keys(displayedItem).length > 0 ? show?.ingredients.split('/') : [];
+  const dir = Object.keys(displayedItem).length > 0 ? show?.directions.split('/') : [];
+  const keys = Object.keys(displayedItem).length > 0 ? Object.keys(show) : [];
   return (
     <div className="container">
-      {/* {console.log(recipe, ingredients, directions)} */}
-      {/* {console.log(localData, 'myinfo')} */}
+       {/* {console.log(show, 'show', displayedItem)} */}
       <form onSubmit={handleSubmit}>
         <label id='recipe'>Recipe</label>
-        <input onChange={handleRecipe} type='text' value={recipe} />
+        <input placeholder='Recipe name' onChange={handleRecipe} type='text' value={recipe} />
         <label id='ingredients'>Ingredients</label>
-        <textarea onChange={handleIng} id='ingredients' value={ingredients} />
+        <textarea placeholder='Separate each ingredient with a "/": Oil / pepper / tomatoes' onChange={handleIng} id='ingredients' value={ingredients} />
         <label id='directions'>Directions</label>
-        <textarea onChange={handleDir} id='directions' value={directions} />
+        <textarea placeholder='Separate each step with a "/": Add two spoons of powdered pepper. / Add 10ml of oil. ' onChange={handleDir} id='directions' value={directions} />
         {isEdit ? <button onClick={submitEdit} type='button'>Submit</button> : <button type='submit'>Add</button>}
       </form>
       {localData.map((item) =>{
@@ -109,10 +115,33 @@ function App() {
       
       <div>
         <p>
-          {show.recipe} <button onClick={handleDelete} id={show.id} type='button'>Del</button>
-          <button type='button' onClick={handleEdit} id={show.id}>Edit</button>
+          {show?.recipe} <button onClick={handleDelete} id={show?.id} type='button'>Del</button>
+          <button type='button' onClick={handleEdit} id={show?.id}>Edit</button>
         </p>
-        {show.ingredients}, {show.directions}
+        {/* {show?.ingredients}, {show?.directions}  */}
+        <div>
+          <p>{keys[1]}</p>
+          <ul>
+            {ingr.map((item) =>{
+            return(
+              // <ul key={item}>
+                <li key={item}>{item}</li>
+              // </ul>
+            )
+          })}
+          </ul>
+          
+          <p>{keys[2]}</p>
+          <ol>
+            {dir.map((item) =>{
+            return(
+              // <ol key={item}>
+                <li key={item}>{item}</li>
+              // </ol>
+            )
+          })}
+          </ol>
+        </div>
       </div>
     </div>
   );
